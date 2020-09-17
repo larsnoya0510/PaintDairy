383ed7db59b452aa7d369279ac40552346e2f323
Noya <larsnoya@gmail.com>
2020-09-17 12:05:01 +0800

    feat :  preview feature

--- app/src/main/java/com/example/paintdairy/fragmentpool.kt
+++ app/src/main/java/com/example/paintdairy/fragmentpool.kt
@@ -4,5 +4,7 @@ class FragmentPool {
     val mDrawListFragment :DrawListFragment by lazy { DrawListFragment.newInstance()}
     val mDrawFragment :DrawFragment by lazy { DrawFragment.newInstance()}
     val mColorPickerFragment :ColorPickerFragment by lazy { ColorPickerFragment.newInstance()}
+    val mPreViewFragment :PreViewFragment by lazy { PreViewFragment.newInstance()}
+
 }
 
